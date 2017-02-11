f5990d44d6fcc27535cb1aad6a09605ae1078481
Liljan <marcus.t.lilja@gmail.com>
2017-02-11 18:04:32 +0100

    Pulsating with frequence. Time based.

--- Automagic Muzzle Flash/Pulse.js
+++ Automagic Muzzle Flash/Pulse.js
@@ -1,17 +1,25 @@
 // Pulse wave function for opacity
 
-//Get selected layer
-//edit its opacity
-
 var layer = app.project.activeItem.selectedLayers[0];
 
 var myProperty = layer.opacity;
 
-var startTime = 2.0;
-var endTime = 4.0;
+var startTime = 0.0;
 var deltaTime = 0.01;
 
-myProperty.setValueAtTime(startTime - deltaTime, 100);
-myProperty.setValueAtTime(startTime, 0);
-myProperty.setValueAtTime(endTime - deltaTime, 0);
-myProperty.setValueAtTime(endTime,100);
+var RPM = 600.0;
+var roundsPerSecond = RPM / 60.0;
+var period = 1.0 / roundsPerSecond; // 
+
+var ammo = 32;
+
+for(var i = 0; i < ammo; i++)
+{
+    myProperty.setValueAtTime(startTime, 100); // 1
+    startTime += period/2.0;
+    myProperty.setValueAtTime(startTime, 100); // 2
+    myProperty.setValueAtTime(startTime +deltaTime , 0); // 3
+    
+    startTime += period/2.0;
+    myProperty.setValueAtTime(startTime - deltaTime , 0); // 4
+}
