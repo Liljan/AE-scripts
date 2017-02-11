550a50f7a366b22abd22394c03a68f8f5f7506ca
Liljan <marcus.t.lilja@gmail.com>
2017-02-11 18:09:50 +0100

    Frame based.

--- Automagic Muzzle Flash/Pulse.js
+++ Automagic Muzzle Flash/Pulse.js
@@ -4,8 +4,12 @@ var layer = app.project.activeItem.selectedLayers[0];
 
 var myProperty = layer.opacity;
 
+// constants
+var RPM = 600.0;
+var FPS = 25.0;
+var deltaTime = 1.0 / FPS;
+
 var startTime = 0.0;
-var deltaTime = 0.01;
 
 var RPM = 600.0;
 var roundsPerSecond = RPM / 60.0;
@@ -18,7 +22,7 @@ for(var i = 0; i < ammo; i++)
     myProperty.setValueAtTime(startTime, 100); // 1
     startTime += period/2.0;
     myProperty.setValueAtTime(startTime, 100); // 2
-    myProperty.setValueAtTime(startTime +deltaTime , 0); // 3
+    myProperty.setValueAtTime(startTime + deltaTime , 0); // 3
     
     startTime += period/2.0;
     myProperty.setValueAtTime(startTime - deltaTime , 0); // 4
